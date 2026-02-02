ebdc52ee6b86417d160ba3a8730eefc2d912064a
Ayushi Gupta <166694642+Ayushi08gupta@users.noreply.github.com>
2026-02-02 23:31:10 +0530

    [Ayushi] : Program to find the profit or loss is added

---
+++ core-java-practice/Basics/level1/Question4.java
@@ -0,0 +1,10 @@
+class Question4 {
+public static void main(String args[]){
+int cp = 129;
+int sp = 191;
+int profit = sp - cp ;
+double profitPercentage = ((double)profit / cp) * 100;
+ System.out.println(" The Cost Price is INR " + cp +" and Selling Price is INR " + sp );
+ System.out.println(" The Profit is INR " + profit +" and Profit Percentage is " + profitPercentage);
+}
+}
