18ce81c1031eb62117ba82bde8a5c5fb9b54d7d1
Ayushi Gupta <166694642+Ayushi08gupta@users.noreply.github.com>
2026-02-02 23:29:54 +0530

    [Ayushi] : Program to convert km to miles

---
+++ core-java-practice/Basics/level1/Question3.java
@@ -0,0 +1,7 @@
+class Question3 {
+public static void main(String args[]){
+double distanceInKm = 10.8;
+double distanceInMile  = 10.8 / 1.6;
+ System.out.println(" The Distance " + distanceInKm +"Km in miles is " + distanceInMile );
+}
+}
