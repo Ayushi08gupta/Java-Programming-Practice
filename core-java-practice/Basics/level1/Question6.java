e64f61dd64010b46315050a4bbcd118af92790ab
Ayushi Gupta <166694642+Ayushi08gupta@users.noreply.github.com>
2026-02-02 23:34:23 +0530

    [Ayushi] : Program to find the discount is added

---
+++ core-java-practice/Basics/level1/Question6.java
@@ -0,0 +1,8 @@
+class Question6 {
+public static void main(String args[]){
+int fee = 125000;
+int discount = 10;
+double amount = fee * discount / 100;
+ System.out.println(" The discount amount is INR " + fee +" and final discounted fee is INR " + amount );
+}
+}
