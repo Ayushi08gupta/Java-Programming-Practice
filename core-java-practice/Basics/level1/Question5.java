a225f2350f33d034f3453cec5bd48c3aff8eed92
Ayushi Gupta <166694642+Ayushi08gupta@users.noreply.github.com>
2026-02-02 23:32:18 +0530

    [Ayushi] : Program to Distribute pen among students

---
+++ core-java-practice/Basics/level1/Question5.java
@@ -0,0 +1,9 @@
+class Question5 {
+public static void main(String args[]){
+int pen = 14;
+int students = 3;
+int distribution = pen / students;
+int remaining = pen % students;
+ System.out.println(" The pen per Student is " + distribution +" and the remaining pen not distributed is " + remaining );
+}
+}
