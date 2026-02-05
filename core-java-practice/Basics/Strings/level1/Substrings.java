f247ff2fcce56d2352cbfc112c029061a496343f
Ayushi Gupta <166694642+Ayushi08gupta@users.noreply.github.com>
2026-02-05 14:37:25 +0530

    [Ayushi] : Program to find substrings is added

---
+++ core-java-practice/Basics/Strings/level1/Substrings.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Substrings{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+	         // Taking User input
+		System.out.println("Enter Strings to Make its Substring");
+		String s = sc.next();
+		arr[] String = s.Substring();
+		System.out.println(arr);
+	       											
+	}
+}
