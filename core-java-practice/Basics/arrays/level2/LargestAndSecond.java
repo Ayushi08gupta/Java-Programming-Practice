95549bc2bf71c6f23927b53395c46fbd371e3d45
Ayushi Gupta <166694642+Ayushi08gupta@users.noreply.github.com>
2026-02-06 22:21:28 +0530

    [Ayushi] : Program to find the largest and second largest number is added

---
+++ core-java-practice/Basics/arrays/level2/LargestAndSecond.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class LargestAndSecond{
+	int a =10;
+public static void show(){
+	LargestAndSecond b = new LargestAndSecond() ;
+	System.out.println(b.a);
+	
+}
+	 }
