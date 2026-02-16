b9acd1c8dd68ccccac51c082c45c8b0e3edbc786
Ayushi08gupta <ayushigupta0523@gmail.com>
2026-02-16 08:50:39 +0530

    Delete core-java-practice directory

--- core-java-practice/Basics/level1/DistanceConversion.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class DistanceConversion{
-	public static void main(String args[]){
-	Scanner sc = new Scanner(System.in);
-	int distance = sc.nextInt();
-	double distanceInMiles = distance / 1.6;
-	System.out.println("The total miles is " + distanceInMiles + " mile for the given " + distance + " Km " );
-}
-}
--- core-java-practice/Basics/level1/VolumeEarth.java
+++
@@ -1,8 +0,0 @@
-class VolumeEarth{
-public static void main(String args[]){
-int radius = 6378;
-double volumeInKm = 4/3.0 * 3.14 * radius *radius*radius;
-double volumeInMiles =  volumeInKm / 1.6 ;
-System.out.println("The Volume of earth in cubic kilometer is " + volumeInKm + " and cubic miles is " + volumeInMiles );
-}
-}
